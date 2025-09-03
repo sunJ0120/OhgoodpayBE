0feea366983200ba8093e687b7c49e677219ddc1
minsss0726 <wndtmgld5551@gmail.com>
2025-09-03 11:19:44 +0900

    [minjung] feat: 초기 설정 v.1.0.0

---
+++ src/main/java/com/ohgoodteam/ohgoodpay/OhgoodpayApplication.java
@@ -0,0 +1,13 @@
+package com.ohgoodteam.ohgoodpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhgoodpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhgoodpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohgoodteam/ohgoodpay/OhgoodpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgoodteam.ohgoodpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhgoodpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
