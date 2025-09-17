c2cd749897d99d39682313c25442d2a5caaa6cac
sunJ0120 <sspure1214@gmail.com>
2025-09-17 19:44:01 +0900

    [sunjung] feat: redis에 상품 저장하는 로직 추가 & 최대 3개씩 꺼내오게 함 (카드 스와이프 식으로 구현하려고)

--- src/main/java/com/ohgoodteam/ohgoodpay/recommend/util/CacheSpec.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/recommend/util/CacheSpec.java
@@ -15,24 +15,29 @@ import java.time.Duration;
  * 3) 취미(string) - hobby
  * 4) 기분 (string) - mood
  * 5) 대화 요약본 (string) - summary
+ * 6) 플로우 (string) - flow
+ * 7) 카운트 (string) - count
+ * 8) 추천 상품 (productDTO) - products
  */
 @Getter
 @RequiredArgsConstructor
 public enum CacheSpec {
-    // 1) 사용자 스냅샷(JSON), 12시간
+    // 1) 사용자 스냅샷(JSON) - 12시간, 유저 기반
     CUSTOMER("v1:customer", Duration.ofHours(12)),
-    // 2) 잔액(String) - 3분
+    // 2) 잔액(String) - 3분, 유저 기반
     BALANCE("v1:balance", Duration.ofMinutes(3)),
-    // 3) 취미(List<String> or Set<String>) - 5분
+    // 3) 취미(List<String> or Set<String>) - 5분, 유저 기반
     HOBBY("v1:hobby", Duration.ofMinutes(5)),
-    // 4) 기분 데이터(String) - 10분, 세션 초기화 시 초기화 하도록 구성
+    // 4) 기분 데이터(String) - 10분, 세션 기반
     MOOD("v1:mood", Duration.ofMinutes(10)),
-    // 5) 대화 요약본 - 1시간, 세션 초기화 시 초기화 하도록 구성
+    // 5) 대화 요약본 - 1시간, 세션 기반
     SUMMARY("v1:summary", Duration.ofHours(1)), //메세지들 캐싱을 위해서 선언
-    // 6) 플로우 - 1시간, 세션 초기화 시 초기화 하도록 구성
+    // 6) 플로우 - 1시간, 세션 기반
     FLOW("v1:flow", Duration.ofHours(1)), //플로우 캐싱을 위해 선언
-    // 7) 카운트 - 1시간, 세션 초기화 시 초기화 하도록 구성
-    COUNT("v1:count", Duration.ofHours(1)); //플로우 카운트를 위해 선언
+    // 7) 카운트 - 1시간, 세션 기반
+    COUNT("v1:count", Duration.ofHours(1)), //플로우 카운트를 위해 선언
+    // 8) 추천 상품 - 1시간, 세션 기반
+    PRODUCTS("v1:products", Duration.ofHours(1)); //추천 상품을 저장하기 위해 선언
 
     private final String prefix; //키 네임 스페이스
     private final Duration ttl;
