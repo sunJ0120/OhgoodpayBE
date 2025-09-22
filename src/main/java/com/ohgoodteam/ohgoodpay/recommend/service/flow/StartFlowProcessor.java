25f198e78f68fdad0215bfcfe05dc153a2b71b78
sunJ0120 <sspure1214@gmail.com>
2025-09-22 15:50:36 +0900

    [sunjung] refactor: start flow 로직 정의 완료

--- src/main/java/com/ohgoodteam/ohgoodpay/recommend/service/flow/StartFlowProcessor.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/recommend/service/flow/StartFlowProcessor.java
@@ -1,20 +1,69 @@
 package com.ohgoodteam.ohgoodpay.recommend.service.flow;
 
+import com.ohgoodteam.ohgoodpay.recommend.dto.CustomerContextWrapper;
 import com.ohgoodteam.ohgoodpay.recommend.dto.FlowContext;
 import com.ohgoodteam.ohgoodpay.recommend.dto.datadto.llmdto.BasicChatResponseDTO;
+import com.ohgoodteam.ohgoodpay.recommend.service.ChatCacheService;
+import com.ohgoodteam.ohgoodpay.recommend.service.ValidCheckService;
+import com.ohgoodteam.ohgoodpay.recommend.service.fastapi.LlmService;
+import com.ohgoodteam.ohgoodpay.recommend.util.flow.ChatFlowType;
+import com.ohgoodteam.ohgoodpay.recommend.util.flow.FlowConfig;
+import com.ohgoodteam.ohgoodpay.recommend.util.flow.FlowConfiguration;
 import com.ohgoodteam.ohgoodpay.recommend.util.flow.FlowType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Slf4j
+@Transactional
+@RequiredArgsConstructor
 public class StartFlowProcessor implements FlowProcessor{
+    private final ChatCacheService chatCacheService; // 캐싱 서비스
+    private final FlowConfiguration flowConfiguration; // next flow 정의
+    private final LlmService llmService; //llm에 요청 보내기 위함
+
     @Override
     public BasicChatResponseDTO process(FlowContext context) {
         // 1. 다음 플로우로 전환
+        FlowConfig config = flowConfiguration.getFlowConfig(context.getCurrentFlow());
+        ChatFlowType nextFlow = config.getNextFlow();
+        chatCacheService.saveFlowBySession(context.getSessionId(), nextFlow.getValue()); //사용을 위해 다음 플로우를 저장
+        chatCacheService.saveCntBySession(context.getSessionId(), 1); //카운트가 있었다면 초기화
 
         // 2. 다음 플로우에 대한 질문 생성
-        return null;
+        CustomerContextWrapper contextWrapper = collectCacheData(context);
+
+        return llmService.generateChat(
+                context.getSessionId(),
+                contextWrapper.getCustomerInfo(),
+                contextWrapper.getMood(),
+                contextWrapper.getHobby(),
+                contextWrapper.getBalance(),
+                context.getMessage(),
+                contextWrapper.getSummary(),
+                nextFlow.getValue()
+        );
     }
 
     @Override
     public boolean canHandle(FlowType flowType) {
-        return false;
+        return FlowType.START.equals(flowType);
+    }
+
+    /**
+     * 캐시된 데이터 수집
+     *
+     * 유효성 검증된 후 진행하므로 context에서 주입해준다.
+     */
+    private CustomerContextWrapper collectCacheData(FlowContext context){
+        return CustomerContextWrapper.builder()
+                .customerInfo(chatCacheService.getCustomerInfo(context.getCustomerId()))
+                .hobby(chatCacheService.getHobby(context.getCustomerId()))
+                .mood(chatCacheService.getMoodBySession(context.getSessionId()))
+                .balance(chatCacheService.getBalance(context.getCustomerId()))
+                .summary(chatCacheService.getSummaryBySession(context.getSessionId()))
+                .build();
     }
 }
--- src/main/java/com/ohgoodteam/ohgoodpay/recommend/util/flow/FlowType.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/recommend/util/flow/FlowType.java
@@ -4,6 +4,7 @@ package com.ohgoodteam.ohgoodpay.recommend.util.flow;
  * ChatFlowType을 큰 틀에서 관리하기 위한 ENUM 클래스
  */
 public enum FlowType {
+    START, // 시작 플로우
     QUESTION,     // 다음 질문을 생성하는 플로우
     RESPONSE,     // 현재 입력에 대한 응답을 생성하는 플로우
     TRANSITION    // 단순 전환만 하는 플로우
