fb87b29284ff78b48cb3d622a0e77aacb104124a
sunJ0120 <sspure1214@gmail.com>
2025-09-28 20:59:17 +0900

    [sunjung] feat: 세션 기반 redis 정보 삭제하는 controller 및 서비스 구현

--- src/main/java/com/ohgoodteam/ohgoodpay/recommend/controller/ChatController.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/recommend/controller/ChatController.java
@@ -1,7 +1,9 @@
 package com.ohgoodteam.ohgoodpay.recommend.controller;
 
+import com.ohgoodteam.ohgoodpay.common.dto.ClearSessionRequestDTO;
 import com.ohgoodteam.ohgoodpay.recommend.dto.ChatStartRequestDTO;
 import com.ohgoodteam.ohgoodpay.recommend.dto.datadto.llmdto.BasicChatResponseDTO;
+import com.ohgoodteam.ohgoodpay.recommend.service.ChatCacheService;
 import com.ohgoodteam.ohgoodpay.recommend.service.ChatService;
 import com.ohgoodteam.ohgoodpay.recommend.util.ApiResponseWrapper;
 import com.ohgoodteam.ohgoodpay.security.util.JWTUtil;
@@ -9,7 +11,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/chat")
 public class ChatController {
     private final ChatService chatService;
+    private final ChatCacheService chatCacheService;
     private final JWTUtil jwtUtil; //jwtUtil 사용하는 방식으로 변경
 
     @PostMapping("")
@@ -56,4 +58,27 @@ public class ChatController {
             return ApiResponseWrapper.error(500, "서버 내부 오류가 발생했습니다");
         }
     }
+
+    // 초기화나 화면을 떠날 시, 세션 기반 정보들을 전부 삭제하기 위함
+    @PostMapping("/clear-session")
+    public ApiResponseWrapper<Void> clearSession(@RequestBody ClearSessionRequestDTO clearSessionRequest, HttpServletRequest request) {
+        try {
+            Long customerId = Long.parseLong(jwtUtil.extractCustomerId(request));
+
+            log.info("-------서버로 들어온 세션 아이디 : {}---------",clearSessionRequest.getSessionId());
+            chatCacheService.clearSessionCache(clearSessionRequest.getSessionId());
+
+            return ApiResponseWrapper.ok(null);
+        } catch (IllegalArgumentException e) {
+            return ApiResponseWrapper.error(400, e.getMessage());
+        } catch (RuntimeException e) {
+            // 로깅만 추가해서 추적 가능하도록 설정
+            log.warn("Chat service runtime error: {}", e.getMessage());
+            return ApiResponseWrapper.error(500, "일시적인 서비스 오류가 발생했습니다");
+        } catch (Exception e) {
+            log.error("Unexpected error in chat controller", e);
+            return ApiResponseWrapper.error(500, "서버 내부 오류가 발생했습니다");
+        }
+    }
+
 }
