7da41fe567314240f7f294ec64e1aeb47621766a
sunJ0120 <sspure1214@gmail.com>
2026-01-14 23:43:22 +0900

    feat: 필요 없는 파일 삭제 및 코드 정리

--- src/main/java/com/ohgoodteam/ohgoodpay/OhgoodpayApplication.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/OhgoodpayApplication.java
@@ -2,13 +2,11 @@ package com.ohgoodteam.ohgoodpay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class OhgoodpayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OhgoodpayApplication.class, args);
 	}
-
 }
 
