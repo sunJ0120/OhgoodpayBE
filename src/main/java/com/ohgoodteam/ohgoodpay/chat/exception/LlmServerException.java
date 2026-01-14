e68151024ce8666d11efe878835ce272a9e921ee
sunJ0120 <sspure1214@gmail.com>
2026-01-14 22:59:55 +0900

    feat: LlmServer관련 예외 처리 다시 추가

---
+++ src/main/java/com/ohgoodteam/ohgoodpay/chat/exception/LlmServerException.java
@@ -0,0 +1,11 @@
+package com.ohgoodteam.ohgoodpay.chat.exception;
+
+public class LlmServerException extends RuntimeException {
+    public LlmServerException(String message) {
+        super(message);
+    }
+
+    public LlmServerException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
