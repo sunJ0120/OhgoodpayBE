9bbff31c6eeba297e2d3cc40a19270a207acfcdf
sunJ0120 <sspure1214@gmail.com>
2026-01-14 15:17:22 +0900

    feat: redis에 history로 저장하기 위한 dto 생성

--- src/main/java/com/ohgoodteam/ohgoodpay/chat/dto/ChatMessage.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/chat/dto/ChatMessage.java
@@ -1,4 +1,14 @@
 package com.ohgoodteam.ohgoodpay.chat.dto;
 
-public class ChatMessage {
+public record ChatMessage (
+        String role,
+        String content
+) {
+    public static ChatMessage userContent(String content) {
+        return new ChatMessage("user", content);
+    }
+
+    public static ChatMessage assistantContent(String content) {
+        return new ChatMessage("assistant", content);
+    }
 }
