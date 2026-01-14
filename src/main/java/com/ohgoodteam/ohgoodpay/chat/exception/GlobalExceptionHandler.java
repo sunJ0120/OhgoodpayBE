ae5e2a00aaaa13bacf0068aa868a3df28c8d43ac
sunJ0120 <sspure1214@gmail.com>
2026-01-14 16:17:00 +0900

    feat: controller에서 발생하는 exception들 처리하기 위한 handler 구현

--- src/main/java/com/ohgoodteam/ohgoodpay/chat/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/chat/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.ohgoodteam.ohgoodpay.chat.exception;
 
-import com.ohgoodteam.ohgoodpay.recommend.util.ApiResponseWrapper;
+import com.ohgoodteam.ohgoodpay.chat.util.ApiResponseWrapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
