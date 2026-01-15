ef399ab5349802035e4d40c27339a9ae0f801414
sunJ0120 <sspure1214@gmail.com>
2026-01-15 23:34:13 +0900

    fix: dto에 예외 처리 추가 및 기본 가격대인 3만원 설정:

--- src/main/java/com/ohgoodteam/ohgoodpay/chat/dto/ProductDto.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/chat/dto/ProductDto.java
@@ -10,9 +10,17 @@ public record ProductDto(
         String category
 ) {
     public static ProductDto of(NaverProduct product) {
+        int price;
+
+        try {
+            price = Integer.parseInt(product.lprice());
+        } catch (NumberFormatException | NullPointerException e) {
+            price = 30000;    // 기본값 설정
+        }
+
         return new ProductDto(
                 product.title().replaceAll("<[^>]*>", ""),  // HTML 태그 제거
-                Integer.parseInt(product.lprice()),
+                price,
                 product.image(),
                 product.link(),
                 product.category1()
