7086d4c5a234336579ef2c08cb1136a87736f1ee
sunJ0120 <sspure1214@gmail.com>
2026-01-15 13:16:46 +0900

    fix: spring 4.0.0 버전에 맞게 GenericJacksonJsonRedisSerializer로 변경

--- src/main/java/com/ohgoodteam/ohgoodpay/config/RedisConfig.java
+++ src/main/java/com/ohgoodteam/ohgoodpay/config/RedisConfig.java
@@ -4,12 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-/**
- * Redis 설정 클래스
- */
 @Configuration
 public class RedisConfig {
 
@@ -21,7 +18,7 @@ public class RedisConfig {
 
         //Key는 String, value는 json으로 직렬화
         StringRedisSerializer stringSer = new StringRedisSerializer();
-        GenericJackson2JsonRedisSerializer jsonSer = new GenericJackson2JsonRedisSerializer();
+        GenericJacksonJsonRedisSerializer jsonSer = GenericJacksonJsonRedisSerializer.builder().build();
 
         //기본 설정 되어있는 redis 바이너리를 변경
         template.setKeySerializer(stringSer);
